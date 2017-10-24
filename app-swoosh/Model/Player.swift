24600b010a6f46870359b26ac15a0010a90e9348
Manohar Kurapati <kvmanohar@gmail.com>
2017-10-24 22:02:36 +0100

    Created IB actions for the buttons and Structure

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Manohar Kurapati on 24/10/2017.
+//  Copyright © 2017 Manosoft. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
